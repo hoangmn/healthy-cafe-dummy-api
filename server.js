42310b3e063f39b7572769875f933e4302ceb4e6
hoangmn <hoangmn@vnpt.vn>
2020-12-10 15:56:31 +0700

    Dummy api commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var jsonServer = require('json-server');
+var server = jsonServer.create();
+var router = jsonServer.router('db.json');
+var middlewares = jsonServer.defaults();
+var fs = require('fs');
+var port = process.env.PORT || 3000;
+
+server.use(middlewares);
+
+server.use(jsonServer.rewriter({
+  "/routes": "/routes",
+  "/products": "/products",
+  "/categories/:category": "/categories/:category",
+  "/categories/:category/:resource": "/:resource?categories=:category"
+}));
+
+server.use(router);
+
+server.listen(port, function () {
+  console.log('\x1b[36mjson-server is running! PORT: ' + port);
+});
